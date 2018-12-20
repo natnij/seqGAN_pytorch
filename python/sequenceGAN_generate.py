890034753a3ea0417dbf81add3f38bd77fcd8e9a
t00421613 <tanjing9@huawei.com>
2018-12-20 11:35:13 +0800

    minor changes.

--- python/sequenceGAN_generate.py
+++ python/sequenceGAN_generate.py
@@ -5,12 +5,12 @@ Created on Thu Dec 20 11:14:08 2018
 import sys
 import torch
 from config import PATH, openLog
+from data_processing import decode
 
 def main(batch_size=1):
     model = torch.load(PATH+'generator.pkl')
     reverse_vocab = torch.load(PATH+'reverse_vocab.pkl')
-    from data_processing import decode
-    
+
     num = model.generate(batch_size=batch_size)
     log = openLog('genTxt_predict.txt')
     result = decode(num, reverse_vocab, log)
